94b092cbcff8b9666a0f618f4b0532d1408d572e
lambda-drama <maniajrmania19@gmail.com>
2025-09-13 10:58:22 +0300

    feat: UPdate budget

---
+++ norwa_ext/norwa_extension/doctype/norwa_budget/norwa_budget.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Norwa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NorwaBudget(Document):
+	pass
---
+++ norwa_ext/norwa_extension/doctype/norwa_budget/test_norwa_budget.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Norwa and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNorwaBudget(FrappeTestCase):
+	pass
